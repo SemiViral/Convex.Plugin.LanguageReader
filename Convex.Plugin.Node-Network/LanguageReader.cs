b68b0aefd1f9f496b5942670781e47e1479d9db1
Antonio DiNostri <semiviral@gmail.com>
2018-04-16 23:07:22 -0500

    IPlugin interface implement
    Basic IPlugin interface now structured.

--- Convex.Plugin.Node-Network/LanguageReader.cs
+++ Convex.Plugin.Node-Network/LanguageReader.cs
@@ -1,3 +1,37 @@
-﻿namespace Convex.Plugin.Node_Network {
-    public class LanguageReader : IPlugin { }
+﻿using System;
+using System.Reflection;
+using System.Threading.Tasks;
+using Convex.Event;
+using Convex.Plugin.Event;
+
+namespace Convex.Plugin.Node_Network {
+    public class LanguageReader : IPlugin {
+        #region MEMBERS
+
+        public string Name => "LanguageReader";
+        public string Author => "Antonio DiNostri";
+        public Version Version => new AssemblyName(GetType().GetTypeInfo().Assembly.FullName).Version;
+        public string Id => Guid.NewGuid().ToString();
+        public PluginStatus Status { get; } = PluginStatus.Stopped;
+
+        #endregion
+
+        #region INTERFACE IMPLEMENTATION
+
+        public Task Start() {
+            throw new NotImplementedException();
+        }
+
+        public Task Stop() {
+            throw new NotImplementedException();
+        }
+
+        public Task Call_Die() {
+            throw new NotImplementedException();
+        }
+
+        public event AsyncEventHandler<PluginActionEventArgs> Callback;
+
+        #endregion
+    }
 }
