9c0513354bbe8485487fbae5e6b1298f42bef276
Antonio DiNostri <semiviral@gmail.com>
2018-04-18 21:53:59 -0500

    some interface methods
    Implemented Stop(), and consequently added DoCallback() and Log() methods

--- Convex.Plugin.Node-Network/LanguageReader.cs
+++ Convex.Plugin.Node-Network/LanguageReader.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Reflection;
+using System.Runtime.InteropServices.ComTypes;
 using System.Threading.Tasks;
 using Convex.Event;
 using Convex.Plugin.Event;
@@ -18,19 +19,40 @@ namespace Convex.Plugin.Node_Network {
 
         #region INTERFACE IMPLEMENTATION
 
-        public Task Start() {
+        public async Task Start() {
             throw new NotImplementedException();
         }
 
-        public Task Stop() {
-            throw new NotImplementedException();
+        public async Task Stop() {
+            if (Status.Equals(PluginStatus.Running) || Status.Equals(PluginStatus.Processing)) {
+                await Log($"Stop called but process is still running from: {Name}");
+            } else {
+                await Log($"Plugin stopped: {Name}");
+            }
         }
 
-        public Task Call_Die() {
+        public async Task Call_Die() {
             throw new NotImplementedException();
         }
 
         public event AsyncEventHandler<PluginActionEventArgs> Callback;
+
+        #endregion
+
+        #region METHODS
+
+        private async Task Log(params string[] args) {
+            await DoCallback(this, new PluginActionEventArgs(PluginActionType.Log, string.Join(" ", args), Name));
+        }
+
+        private async Task DoCallback(object sender, PluginActionEventArgs args) {
+            if (Callback == null)
+                return;
+
+            args.PluginName = Name;
+
+            await Callback.Invoke(sender, args);
+        }
 
         #endregion
     }
